acfa90229a2930e84d371949a5bbc8c5626c89b1
Ivan <kavoikovivan@gmail.com>
2026-09-25 18:45:48 +0300

    Android: enable real Smart Route unit tests

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -173,4 +173,5 @@ dependencies {
     implementation("com.google.zxing:core:3.5.3")
     implementation("androidx.webkit:webkit:1.12.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    testImplementation("junit:junit:4.13.2")
 }
